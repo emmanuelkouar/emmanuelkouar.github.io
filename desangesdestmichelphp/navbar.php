c4a9a6fd04315598ca012f2099708dbb78ddd9ac
Emmanuelkouar <emmanuel.kouar@student.kdg.be>
2017-06-08 12:54:18 +0200

    php test on github

---
+++ desangesdestmichelphp/navbar.php
@@ -0,0 +1,25 @@
+  <nav class="navbar navbar-default navbar-fixed-top">
+              <div class="container">
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                  <a class="navbar-brand" href="#">Des anges de St-Michel</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                  <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Accueil</a></li>
+                    <li><a href="#about">Nos niches</a></li>
+                    <li><a href="#contact">Nos chiots</a></li>
+                    <li><a href="#contact">Notre livre d'or</a></li>
+                    <li><a href="#contact">A propos de nous</a></li>
+                  </ul>
+                  <ul class="nav navbar-nav navbar-right">
+                    <li><a href="../navbar/">Contactez nous</a></li>
+                  </ul>
+                </div>
+              </div>
+        </nav>
